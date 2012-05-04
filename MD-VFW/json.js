d8970812284095102f634d2c846fd0ba4d7df81a
ramihollings <ramihollingsworth@gmail.com>
2012-05-03 22:39:50 -0400

    All Sync

---
+++ MD-VFW/json.js
@@ -0,0 +1,35 @@
+// DOCTYPE html
+// Project: Web App Part 4
+// Rami Hollingsworth
+// Term 0112
+
+
+
+var json = {
+	"item1": {
+		"group": ["Group:", "Own"],
+		"name": ["Name of Electronic:", "Samsung Galaxy S2"],
+		"purchased": ["Purchase Date", "2011-9-10"],
+		"rating": ["Rating", "5"],
+		"toe": ["Type of Electronic", "Phone"], 
+		"notes": ["Notes", "Bad Reception"]
+	},
+	"item2": {
+		"group": ["Group:", "Owned"],
+		"name": ["Name of Electronic:", "Samsung Galaxy S1"],
+		"purchased": ["Purchase Date", "2011-02-10"],
+		"rating": ["Rating", "3"],
+		"toe": ["Type of Electronic", "Phone"], 
+		"notes": ["Notes", "Small Screen"]
+	},
+	"item3": {
+		"group": ["Group:", "Own"],
+		"name": ["Name of Electronic:", "Samsung Galaxy Nexus"],
+		"purchased": ["Purchase Date", "2011-12-25"],
+		"rating": ["Rating", "5"],
+		"toe": ["Type of Electronic", "Phone"], 
+		"notes": ["Notes", "Verizon"]
+	}
+
+
+}
